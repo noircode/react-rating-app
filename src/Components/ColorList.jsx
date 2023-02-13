cb2d0ad23620eed5cff0d8497eb14c5980dd44e2
VerticalJoke <aldimaulana9090@gmail.com>
2023-02-13 04:50:33 +0000

    added rating functionality

--- src/Components/ColorList.jsx
+++ src/Components/ColorList.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Color from './Color';
 
-export default function ColorList({ colors = [], onRemoveColor = f => f }) {
+export default function ColorList({ colors = [], onRemoveColor = f => f, onRateColor= f => f }) {
   if (!colors.length) return <div>No Colors Listed</div>;
   return (
     <div>
       {
-        colors.map(color => <Color 
+        colors.map(color => 
+        <Color 
           key={color.id} {...color}
-          onRemove={onRemoveColor} /> ) 
+          onRemove={onRemoveColor}
+          onRate={onRateColor}
+          {...color} /> ) 
       }
     </div>
   )
